8453e1a7d0c06311aaf6d57d8d0cd53b79f09b70
Alison <roa.alisonn@gmail.com>
2022-12-21 15:34:07 -0300

    Shopping proyect

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import './styles/cart.css'
+
 
 const Cart = ({ cart, setCart, handleChange }) => {
   const { price } = cart;
@@ -22,29 +22,30 @@ const Cart = ({ cart, setCart, handleChange }) => {
   });
 
   return (
-    <article className="absolute -top-3 left-0 w-full min-h-screen bg-gray-100 flex flex-col justify-center gap-4">
+    <article className="absolute top-[48px] left-0 w-full min-h-screen bg-gray-100 flex flex-col justify-center gap-4 ">
       {cart.map((item) => (
-        <div className="container mx-auto ml-2 mt-3" key={item.id}>
-          <div className="flex ">
-            <img src={item.img} alt="" className="w-[50px] h-[60px]" />
+        <div className="container mx-auto ml-2 mt-5" key={item.id}>
+          <div className="flex sm:container mx-auto sm:ml-[20px]">
+            <img src={item.img} alt="" className="w-[50px] h-[60px] sm:w-[80px] sm:h-[80px]" />
             <p className="font-bold ml-2 text-sm mb-0">{item.title}</p>
           </div>
 
-          <div className="flex ml-[60px] space-x-2 font-medium -mt-6">
+          <div className="flex ml-[60px] space-x-2 font-medium -mt-6 sm:ml-[650px]">
             <button onClick={() => handleChange(item, 1)} className="bg-gray-300 font-bold w-5">+</button>
             <button className="bg-gray-300 w-5">{item.amount}</button>
             <button onClick={() => handleChange(item, -1)} className="bg-gray-300 font-bold w-5">-</button>
           </div>
 
-          <div className="flex justify-center -mt-6">
+          <div className="flex justify-center -mt-6 sm:ml-[750px]">
             <span className="font-bold ml-[180px] -mb-[100px]">{"$" + item.price}</span>
             <button className="bg-[#ffad79] ml-2" onClick={() => handleRemove(item.id)}>Remove</button>
           </div>
         </div>
       ))}
-      <div className="font-bold text-lg ml-10 mt-10 sm:ml-5">
-        <span>Total Price of your Cart</span>
-        <span className="bg-green-300 ml-5">${prices}</span>
+      
+      <div className="font-bold text-lg ml-10 mt-10 mb-3  sm:ml-[500px]">
+        <span >Total Price of your Cart</span>
+        <span className="bg-green-300 ml-5  ">${prices}</span>
       </div>
     </article>
   );
--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -4,13 +4,14 @@ import Items from "./Items";
 
 const ListofItems = ({ handleClick }) => {
   return (
-    <div className=" container mx-auto">
+    <div className="container mx-auto w-full">
 
-      <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid justify-items-center grid-cols-1 mt-10 mb-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
       {data.map((item) => {
         return <Items key={item.id} item={item} handleClick={handleClick} />;
       })}
       </div>
+      
     </div>
   );
 };
